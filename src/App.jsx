6571803b3cc5ba8519348652aaba563177c2cf50
ashutoshverma23 <ashutosh.2125cse1094@kiet.edu>
2025-07-22 11:12:29 +0530

    add source code

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl font-bold">Hello world</div>
+    </>
+  );
+}
+
+export default App;
